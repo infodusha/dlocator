461b9f172d60ca784ea6e8ae5321fa928b8fee2d
infodusha <infodusha@ya.ru>
2018-10-29 16:00:38 +0300

    Patch for typescript

--- module.js
+++ module.js
@@ -1,17 +1,15 @@
 module.exports = class Module {
 
+    constructor() {
+        this._dependecies = new Set();
+        this._services = new Map();
+    }
+
     async init(locator) {
         this._locator = locator;
+        
     }
 
     async afterInit() {}
-
-    get _dependecies() {
-        return new Set();
-    }
-
-    get _services() {
-        return new Map();
-    }
 
 }
